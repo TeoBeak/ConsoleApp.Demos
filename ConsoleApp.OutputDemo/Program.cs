159effba9381c179f568fe673a899756ddee61e2
user <user@DESKTOP-Q06PMMD>
2025-08-01 10:35:50 +0900

    초기 커밋.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! My name is TTTT");
+Console.WriteLine("I am from SK!");
+Console.WriteLine("Anything you want to say");
+
+Console.Write("Line 1");
+Console.Write("Line 2");
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Different datatypes
+
+/*
+ *text - string 
+ *integers - int
+ *decimal - double, float, decimal
+ *logical - bool
+ */
+
+string name = "Teo B";
+
+Console.WriteLine(name);
+Console.WriteLine("I am " + name);  // String concatenation
+Console.WriteLine($"They call me {name}");   // String interpolation
+Console.WriteLine("I was given the name {0}", name);   // Formatted String
+
+int age = 36;
+int retirementYearsLeft = 30;
+int retirementAge = age + retirementYearsLeft;
+Console.WriteLine("My age is: " + age);
+Console.WriteLine("My retirement age is: " + retirementAge);
+
+bool isRetired =  false;
+Console.WriteLine("Am I retired? " + isRetired);
