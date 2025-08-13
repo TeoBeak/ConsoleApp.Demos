7f8dcd326558be05376f794d5ddc36271745304d
Teo <kjbs0518@gmail.com>
2025-08-13 17:36:25 +0900

    Add viod, value return methods

---
+++ ConsoleApp,Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("***************** - Methods - *****************");
+
+// void methods - complete a task without returning a value
+void PrintName()
+{
+    // Method code 
+    Console.WriteLine("Teo Baek");
+}
+
+// value returning methods - returns a value after an operation
+int GetFiveYearsAgo()
+{
+    int year = DateTime.Now.AddYears(-5).Year;
+    return year;
+}
+
+// methods with parameters
+
+// methods with optional parameters
+
+// methods with nullable parameters
+
+/* Function Calls */
+PrintName();
+int fiveYearsAgo = GetFiveYearsAgo();
+Console.WriteLine($"Five years ago was: {fiveYearsAgo}");
