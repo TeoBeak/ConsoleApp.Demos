52eda1d29fa1d396534bf3fc6c830552a0e1a4ff
Teo <kjbs0518@gmail.com>
2025-08-14 17:10:15 +0900

    Added arrays and Lists demos

---
+++ ConsoleApp.Arrays/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("************************** - Arrays - **************************");
+
+// Tell me how many students and grades are to be entered
+Console.Write("Please indicate the number of grades to be entered: ");
+int numberOfGrades = Convert.ToInt32(Console.ReadLine());
+
+// Delcare Fixed Size Array
+int[] grades = new int[numberOfGrades];
+string[] students = new string[numberOfGrades];
+// Add values to Fixed Size Array
+//grades[0] = 45;
+//grades[1] = 67;
+//grades[2] = 89;
+//grades[3] = 90;
+//grades[4] = 100;
+
+for (int i = 0; i < numberOfGrades; i++)
+{
+    Console.Write($"Enter Student Name: ");
+    students[i] = Console.ReadLine();
+
+    Console.Write($"Enter Grade: ");
+    grades[i] = Convert.ToInt32(Console.ReadLine());
+} 
+// Print vlaues in Fixed Size Array
+Console.WriteLine("The Grades you have entered are: ");
+for (int i = 0; i < numberOfGrades; i++)
+{
+    Console.WriteLine($"{students[i]}: {grades[i]}");
+}
+
+////Declare variable size array
+//string[] studentNames = {"NewTon", "Einstein", "Curie"};
+
+////Add values to variable size array
+//for (int i = 0; i < studentNames.Length; i++)
+//{
+//    Console.Write($"Enter Student Name: ");
+//    studentNames[i] = Console.ReadLine();
+//}
+
+//// Print values in variable size array
+//Console.WriteLine("The Student Names you have entered are: ");
+//for (int i = 0; i < studentNames.Length; i++)
+//{
+//    Console.WriteLine(studentNames[i]);
+//}
---
+++ ConsoleApp.Lists/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("************************** - Lists - **************************");
+
+// Declare a List
+List<int> grades = new List<int>();
+List<string> students = new List<string>();
+int grade = 0;
+string name;
+int gradeCount = 0;
+int @continue;
+//var grades = new List<int>();
+//List<int> greade = new();
+
+// Add values to list
+//grades.Add(45);
+//grades.Add(67);
+//Console.WriteLine(grades[0]);
+//Console.WriteLine(grades[1]);
+
+do
+{
+    gradeCount+=1;
+    Console.Write("Enter Student Name: ");
+    name = Console.ReadLine();
+    students.Add(name);
+
+    Console.Write("Enter Grade: ");
+    grade = Convert.ToInt32(Console.ReadLine());
+    if(grade != -1)
+    {
+        grades.Add(grade);
+    }
+
+    Console.Write("Do you want to continue? (1 = yes | 2 = no): ");
+    @continue = Convert.ToInt32(Console.ReadLine());
+} while (@continue == 1);
+
+// Print values in list - for
+Console.WriteLine("The Grades you have entered are: ");
+for (int i = 0; i < gradeCount; i++)
+{
+    Console.WriteLine($"{students[i]}: {grades[i]}");
+}
+
+
+//// Print values in list - foreach
+//Console.WriteLine("Printing grades via foreach loop");
+//foreach (int g in grades)
+//{
+//    Console.WriteLine(g);
+//}
