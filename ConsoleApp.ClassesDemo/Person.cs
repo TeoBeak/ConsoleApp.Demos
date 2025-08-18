6279e3e1be8310906bbcc0c5809b385d16407ab5
Teo <kjbs0518@gmail.com>
2025-08-18 20:01:49 +0900

    Added Classes demo(Person class)

---
+++ ConsoleApp.ClassesDemo/Person.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Define a class
+using System.Security.Cryptography;
+
+public class Person
+{
+    // Properties / Data Members
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+
+    // Fields members
+    private string _taxNumber;
+    public void PrintFullName()
+    {
+        Console.WriteLine($"Full Name: {FirstName} {LastName}");
+    }
+
+    public void PrintInitials()
+    {
+        var firstInitial = FirstName[0];
+        var lastInitial = LastName[0];
+        Console.WriteLine($"{firstInitial} {lastInitial}");
+    }
+
+    public void GgenerateTaxNumber()
+    {
+        _taxNumber = RandomNumberGenerator.GetInt32(100000,999999).ToString();
+    }
+
+    public string GetTaxNumber()
+    {
+        return _taxNumber;
+    }
+}
---
+++ ConsoleApp.ClassesDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("************************* - Classes and Object - *************************");
+
+// Define an object of type Person
+
+Person person; // this wiil be null by default
+
+Person baby = new Person();
+baby.FirstName = "Thompson";
+baby.LastName = "Theresa";
+baby.DateOfBirth = new DateOnly(2023,10,29);
+
+baby.PrintFullName();
+baby.PrintInitials();
+baby.GgenerateTaxNumber();
+
+var taxNumber = baby.GetTaxNumber();
+
+Console.WriteLine($"Tax Number: {taxNumber}");
